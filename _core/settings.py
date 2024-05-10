8a4757381fe0c9d9f3d85e28a916d85815654ff8
joabesnts09 <joabesilva1233@gmail.com>
2024-05-10 12:18:34 -0300

    Finalização da entrega semanal"

--- _core/settings.py
+++ _core/settings.py
@@ -41,10 +41,12 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "drf_spectacular",
     "accounts",
     "courses",
     "students_courses",
     "contents",
+    
 ]
 
 MIDDLEWARE = [
@@ -135,3 +137,18 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "accounts.Account"
+
+
+
+REST_FRAMEWORK = {
+
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+
+# _core/settings.py
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Course Management API',
+    'DESCRIPTION': 'Course, teacher and student management',
+    'VERSION': '1.0.0',
+}
